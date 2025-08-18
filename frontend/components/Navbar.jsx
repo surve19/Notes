5610dd6d170ee7462d31d26fb12f010c530ad649
vrutik991 <rathodvrutik123@gmail.com>
2025-08-18 12:26:55 +0530

    Navbar

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='p-4 flex items-center bg-[#03045e]'>
       <img src={icon} className='w-18 h-18'/>
-      <h3 className='text-3xl font-bold ml-2 font-mono text-white'> Notify </h3>
+      <h3 className='text-3xl font-bold ml-2 font-mono text-white'> Notifyy </h3>
     </div>
   )
 }
